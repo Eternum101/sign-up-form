7599fbe68d21de7794599538e27c02e7f699ef4d
Eternum101 <eternumhd111@gmail.com>
2023-05-29 13:55:52 +1000

    Added Comments Finished Project

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+// Declaring variables
 let password = document.getElementById('password');
 let confirmPassword = document.getElementById('confirm-password');
 let result = document.querySelector('.error');
 
+// Checks if password and confirmPassword match and updates result text accordingly
 function checkPassword() {
     result.innerText = password.value == confirmPassword.value ? '' : 'Password Does Not Match!';
 }
 
+// Adds keyup event listener to password element to call checkPassword function if
+// confirmPassword value is not empty
 password.addEventListener('keyup', () => {
     if (confirmPassword.value.length != 0) 
     checkPassword();
 });
 
+// Adds keyup event listener to confirmPassword element to call checkPassword function
 confirmPassword.addEventListener('keyup', checkPassword);
